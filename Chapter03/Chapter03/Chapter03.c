6bca90b3cd7dafedd736624ac8b8d37f76eacebc
Centaucyan <cencyan35@nate.com>
2026-04-01 19:20:53 +0900

    Chapter 03 supplementary study

--- Chapter03/Chapter03/Chapter03.c
+++ Chapter03/Chapter03/Chapter03.c
@@ -2,17 +2,25 @@
 
 void main()
 {
-	/*
-	int nData = 10;
+	//. p.102
+	/*int nData = 10;
 	int *pnData = &nData;
 
 	*pnData = 255;
 	*pnData = 256;
-	*pnData = 9999999999;
-	*/ 
+	*pnData = 9999999999;*/
+	 
 
-	
-	double dblData = 123.45;
+	//. p.84
+	char* pszData = "abc";
+	char ch = 'A';
+
+	printf("%s\n", pszData);
+	printf("%c\n", ch);
+	printf("%s\n", ch);  //. %s는 문자열을 받음. 문자 하나는 %c로 받음.
+
+	//. p.83
+	/*double dblData = 123.45;
 	float flData = -123.45;
 	long lData = 1234.56;
 	int nData = 12345;
@@ -23,7 +31,7 @@ void main()
 	printf("%d\n\n", sizeof(nData));
 
 	printf("%d\n", sizeof(16.61));
-	printf("%d\n", sizeof(16.61f));
+	printf("%d\n", sizeof(16.61f));*/
 
 	// x = 0;
 	
