88fc2eea56addccd6e5b1d560adadb6638458457
Centaucyan <cencyan35@nate.com>
2026-04-11 12:52:10 +0900

    Study if, switch/case statement in C-language

--- Chapter07/Chapter07/Chapter07.c
+++ Chapter07/Chapter07/Chapter07.c
@@ -2,9 +2,45 @@
 
 void main(void)
 {
+	//. p.243
+	float fData = 1.1f;
+	switch (fData)  //. switch문의 식의 결과값은 정수계열(int, char, short)만 올 수 있다.
+	{
+	case 1.1f:
+		break;
+	}
+
+
+	//. p.240
+	//int nInput = 0;
+	//char chCredit = 'X';
+	//scanf_s("%d", &nInput);
+
+	//switch (nInput / 10) {
+	//case 10:
+	//	//break;
+	//case 9:
+	//	chCredit = 'A';
+	//	//break;
+	//case 8:
+	//	chCredit = 'B';
+	//	//break;
+	//case 7:
+	//	chCredit = 'C';
+	//	//break;
+	//case 6:
+	//	chCredit = 'D';
+	//	//break;
+	//default:
+	//	;
+	//	//chCredit = 'F';
+	//}
+
+	//printf("Credit: %c", chCredit);
+
 	//. p.226
-	int nData = 0, nNewData = 20;
+	/*int nData = 0, nNewData = 20;
 
 	if (nNewData = nData)
-		puts("Success");
+		puts("Success");*/
 }
