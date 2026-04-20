c8dae3b7f00a21e3d995fb7678574fdd0ef12e26
Centaucyan <cencyan35@nate.com>
2026-04-20 23:26:50 +0900

    Chapter 7, Supplementary Learning.

--- Chapter07/Chapter07/Chapter07.c
+++ Chapter07/Chapter07/Chapter07.c
@@ -86,7 +86,34 @@ RESTART:
 
 
 	//. p.240
-	//
+	/*int nInput = 0;
+	char chCredit = 'X';
+	scanf_s("%d", &nInput);
+
+	switch (nInput / 10)
+	{
+	case 10:
+	case 9:
+		chCredit = 'A';
+		break;
+
+	case 8:
+		chCredit = 'B';
+		break;
+	case 7:
+		chCredit = 'C';
+		break;
+
+	case 6:
+		;
+		chCredit = 'D';
+		break;
+
+	default:
+		chCredit = 'F';
+	}
+
+	printf("Credit : %c\n", chCredit);*/
 
 
 	//. p.226
