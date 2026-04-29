488ef574407bb0569b413c0fbed0ea5eef104bd8
Centaucyan <cencyan35@nate.com>
2026-04-30 00:09:59 +0900

    Solve the practice question 2 in Chapter 08.

--- Chapter08/Chapter08/Chapter08.c
+++ Chapter08/Chapter08/Chapter08.c
@@ -2,15 +2,28 @@
 
 void main(void)
 {
-	//. p.276-1
-	int n_sum = 0;
-	//int i = 0;
+	//. p.276-2
+	int n_num_4t = 0, n_sum_4t = 0;
 
-	for (int i = 1; i <= 100; ++i) {
-		n_sum += i;
+	for (int i = 10; i <= 100; ++i) {
+		if ((i % 4) == 0) {
+			n_num_4t += 1;
+			n_sum_4t += i;
+		}
 	}
-		
-	printf("sum 1~100: %d", n_sum);
+
+	printf("n_num4t: %d __ n_sum4t: %d\n", n_num_4t, n_sum_4t);
+
+
+	//. p.276-1
+	//int n_sum = 0;
+	////int i = 0;
+
+	//for (int i = 1; i <= 100; ++i) {
+	//	n_sum += i;
+	//}
+	//	
+	//printf("sum 1~100: %d", n_sum);
 
 
 	//. p.269
